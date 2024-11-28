8dea2728522a174dc0cd6ffbf03276f2b5018bb1
Ducker <mateuszczerewko@gmail.com>
2024-11-28 09:01:40 +0100

    wip: adding skip to content button

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,63 +1,65 @@
 import Link from './Link'
+import Image from 'next/image'
 
-const Card = ({ title, description, href, technologyIcons }) => (
-  <div className="w-full sm:w-1/2 p-4">
-    <div className="h-full flex flex-col overflow-hidden rounded-md border-2 border-solid border-gray-200 bg-transparent transition duration-500 hover:scale-105 hover:border-primary-500 hover:bg-gray-300 dark:border-gray-700 dark:hover:border-primary-500 dark:hover:bg-gray-800">
-      <div className="p-6 flex flex-col flex-grow">
-        {/* Icons Section */}
-        <div className="flex items-center justify-between mb-4">
-          {/* Folder Icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            className="h-10 w-10 text-primary-color-500 dark:text-primary-color-dark-500"
-          >
-            <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-          </svg>
-          {/* External Link */}
-          {href && (
-            <a
-              href={href}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-500 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-400"
-              aria-label={`Link to ${title}`}
+export default function Card({ title, description, href, technologyIcons }) {
+  return (
+    <div className="w-full p-4 sm:w-1/2">
+      <div className="flex h-full flex-col overflow-hidden rounded-md border-2 border-solid border-gray-200 bg-transparent transition-transform duration-500 hover:scale-105 hover:border-primary-500 hover:bg-gray-300 dark:border-gray-700 dark:hover:border-primary-500 dark:hover:bg-gray-800">
+        <div className="flex flex-grow flex-col p-6">
+          {/* Icons Section */}
+          <div className="mb-4 flex items-center justify-between">
+            {/* Empty Folder Icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              className="h-10 w-10 text-gray-500 dark:text-gray-400"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 1024 1024"
-                fill="currentColor"
-                className="h-6 w-6"
+              <path d="M4 4h5l2 3h9a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2z"></path>
+            </svg>
+            {/* External Link */}
+            {href && (
+              <a
+                href={href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-500 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-400"
+                aria-label={`Link to ${title}`}
               >
-                <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1C960 276.4 759.7 76.2 511.6 76.3z"></path>
-              </svg>
-            </a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 1024 1024"
+                  fill="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1C960 276.4 759.7 76.2 511.6 76.3z"></path>
+                </svg>
+              </a>
+            )}
+          </div>
+          {/* Content Section */}
+          <div className="flex-grow">
+            <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">{title}</h2>
+            <p className="prose text-gray-500 dark:text-gray-400">{description}</p>
+          </div>
+          {/* Technology Icons */}
+          {technologyIcons && (
+            <div className="mt-4 flex space-x-3">
+              {technologyIcons.map((icon, index) => (
+                <Image
+                  key={index}
+                  src={icon}
+                  alt="Technology Icon"
+                  width={32}
+                  height={32}
+                  className="h-8 w-8"
+                />
+              ))}
+            </div>
           )}
         </div>
-        {/* Content Section */}
-        <div className="flex-grow">
-          <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">{title}</h2>
-          <p className="prose text-gray-500 dark:text-gray-400">{description}</p>
-        </div>
-        {/* Technology Icons */}
-        {technologyIcons && (
-          <div className="mt-4 flex space-x-3">
-            {technologyIcons.map((icon, index) => (
-              <img
-                key={index}
-                src={icon}
-                alt="Technology Icon"
-                className="h-8 w-8"
-                title="Technology"
-              />
-            ))}
-          </div>
-        )}
       </div>
     </div>
-  </div>
-)
-
-export default Card
+  )
+}
--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -1,43 +1,37 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 'use client'
 
 import { useState } from 'react'
-import { slug } from 'github-slugger'
 import { formatDate } from 'pliny/utils/formatDate'
 import { CoreContent } from 'pliny/utils/contentlayer'
 import type { Blog } from 'contentlayer/generated'
 import Link from '@/components/Link'
 import siteMetadata from '@/data/siteMetadata'
 import tagData from 'app/tag-data.json'
+import Image from 'next/image'
 
 interface ListLayoutProps {
   posts: CoreContent<Blog>[]
   title: string
-  initialDisplayPosts?: CoreContent<Blog>[]
 }
 
 export default function ListLayoutWrapper({ posts, title }: ListLayoutProps) {
   const tagCounts = tagData as Record<string, number>
   const sortedTags = Object.keys(tagCounts).sort((a, b) => tagCounts[b] - tagCounts[a])
 
-  // State for the currently selected tag (null = no filter)
   const [selectedTag, setSelectedTag] = useState<string | null>(null)
 
-  // Toggles tag selection for filtering posts
   const toggleTag = (tag: string) => {
     setSelectedTag((prevSelected) => (prevSelected === tag ? null : tag))
   }
 
-  // Filter posts based on selected tag
   const filteredPosts = selectedTag
     ? posts.filter((post) => post.tags.includes(selectedTag))
     : posts
 
   return (
     <div>
-      {/* Page Header */}
-      <h1 className="text-4xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl sm:leading-tight md:text-6xl md:leading-tight mb-8">
-        Blog
+      <h1 className="mb-8 text-4xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-gray-100">
+        {title}
       </h1>
 
       {/* Tags Section */}
@@ -62,19 +56,20 @@ export default function ListLayoutWrapper({ posts, title }: ListLayoutProps) {
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {filteredPosts.map((post) => {
           const { path, date, title, summary, tags, images } = post
-          const postImage = Array.isArray(images) ? images[0] : images // Use first image if multiple
+          const postImage = Array.isArray(images) ? images[0] : images
 
           return (
             <div key={path} className="relative overflow-hidden rounded-lg shadow-md">
               {/* Post Image */}
               <Link href={`/${path}`} aria-label={title}>
                 <div className="relative h-48 bg-gray-200">
-                  <img
+                  <Image
                     src={postImage || '/placeholder-image.jpg'}
                     alt={title}
-                    className="absolute inset-0 h-full w-full object-cover"
+                    layout="fill"
+                    objectFit="cover"
                   />
-                  {/* Tags Displayed on Image */}
+                  {/* Tags on Image */}
                   <div className="absolute bottom-0 left-0 flex flex-wrap gap-2 bg-black/50 p-2">
                     {tags.map((tag) => (
                       <span
