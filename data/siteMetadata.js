99937786989e0fe859e2d1b662887a7ae895bce2
Ducker <mateuszczerewko@gmail.com>
2024-11-30 23:58:21 +0100

    fix: article semantic fix (2x h1 )

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'DuckerQA Blog – Where Testing Gets Quacky',
   author: 'Mateusz Czerewko (Ducker)',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
